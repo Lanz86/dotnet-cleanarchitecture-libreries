a82abed5b9eca18bfe4e1d8e6e0158a89348d6f6
Antonio Lanzolla <antonio.lanzolla@gmail.com>
2024-01-09 10:55:45 +0100

    Added WebApi.nuspec and WebApi.csproj

---
+++ src/WebApi/Infrastructure/EndpointGroupBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace LanzDev.CleanArchitecture.Libraries.WebApi.Infrastructure;
+
+public abstract class EndpointGroupBase
+{
+    public abstract void Map(WebApplication app);
+}
---
+++ src/WebApi/Infrastructure/IEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Ardalis.GuardClauses;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace LanzDev.CleanArchitecture.Libraries.WebApi.Infrastructure;
+
+public static class IEndpointRouteBuilderExtensions
+{
+    public static IEndpointConventionBuilder MapGet(this IEndpointRouteBuilder builder, Delegate handler, string pattern = "")
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        return builder.MapGet(pattern,handler)
+            .WithName(handler.Method.Name);
+    }
+
+    public static IEndpointConventionBuilder MapPost(this IEndpointRouteBuilder builder, Delegate handler, string pattern = "")
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        return builder.MapPost(pattern, handler)
+            .WithName(handler.Method.Name);
+    }
+
+    public static IEndpointConventionBuilder MapPut(this IEndpointRouteBuilder builder, Delegate handler, string pattern)
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        return builder.MapPut(pattern, handler)
+            .WithName(handler.Method.Name);
+    }
+
+    public static IEndpointConventionBuilder MapDelete(this IEndpointRouteBuilder builder, Delegate handler, string pattern)
+    {
+        Guard.Against.AnonymousMethod(handler);
+
+        return builder.MapDelete(pattern, handler)
+            .WithName(handler.Method.Name);
+    }
+}
